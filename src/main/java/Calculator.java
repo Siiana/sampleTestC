91dce824dd8a85d634a875b4c78a85b661affec8
Siiana <siiana-elfiyka@rambler.ru>
2019-09-30 22:06:34 +0300

    add: new test calc

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -17,6 +17,28 @@ public class Calculator {
         return a * b;
     }
 
+    public int division(int a, int b) {
+
+        return a / b;
+    }
+
+    public int subtraction(int a, int b) {
+
+        return a - b;
+    }
+
+    public double squareRoot(int a) {
+
+        return Math.sqrt(a);
+    }
+
+    public double pow2(int a, int b) {
+
+        return Math.pow(a,b);
+    }
+
+
+
     //TODO
     //add methods for multiply, division, subtraction, square root, x^2
 
--- src/test/java/AddTwoValuesTests.java
+++ src/test/java/AddTwoValuesTests.java
@@ -2,6 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static java.lang.Double.NaN;
 import static junit.framework.TestCase.assertEquals;
 
 /**
@@ -26,6 +27,49 @@ public class AddTwoValuesTests {
     @Test
     public void testAddTwoNegativeValues() {
         assertEquals("-10 + -5 must be -15", -15, calc.add(-10, -5));
+    }
+
+    @Test
+    public void testMultiplyTwoValues() {
+        assertEquals("10 * 5 must be 50", 50, calc.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivisionTwoValues() {
+        assertEquals("10 / 5 must be 2", 2, calc.division(10, 5));
+    }
+
+    @Test
+    public void testNegativeDivisionValue() {
+        String message = "";
+        try {
+            calc.division(10, 0);
+        } catch (ArithmeticException e) {
+            message = e.getMessage();
+        }
+        assertEquals("10 / 0 must be ", "/ by zero", message);
+    }
+
+    @Test
+    public void testSubtractionTwoValues() {
+        assertEquals("10 - 5 must be 5", 5, calc.subtraction(10, 5));
+    }
+
+    @Test
+    public void testSquareRootValue() {
+        assertEquals("4 must be 2", (double) 2, calc.squareRoot(4));
+    }
+
+    @Test
+    public void testNegativeSquareRootValue() {
+        double result = calc.squareRoot(-1);
+            assertEquals("Error",NaN, result);
+    }
+
+
+    @Test
+    public void testPow2Value() {
+        assertEquals("2 must be 4", 4d, calc.pow2(2, 2));
     }
 
     //TODO
